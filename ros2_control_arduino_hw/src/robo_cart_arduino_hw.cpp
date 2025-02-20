f9c8cc1e3031876aadb858c02b6ced3a19d3e918
vamichrom <vamichrom@gmail.com>
2025-02-20 20:04:32 +0300

    Update arduino hardware interface

---
+++ ros2_control_arduino_hw/src/robo_cart_arduino_hw.cpp
@@ -0,0 +1,111 @@
+// Copyright (c) 2025, vamichrom
+// Copyright (c) 2025, Stogl Robotics Consulting UG (haftungsbeschränkt) (template)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <limits>
+#include <vector>
+
+#include "ros2_control_arduino_hw/robo_cart_arduino_hw.hpp"
+#include "hardware_interface/types/hardware_interface_type_values.hpp"
+#include "rclcpp/rclcpp.hpp"
+
+namespace ros2_control_arduino_hw
+{
+hardware_interface::CallbackReturn RoboCartArduinoHW::on_init(
+  const hardware_interface::HardwareInfo & info)
+{
+  if (hardware_interface::SystemInterface::on_init(info) != CallbackReturn::SUCCESS)
+  {
+    return CallbackReturn::ERROR;
+  }
+
+  // TODO(anyone): read parameters and initialize the hardware
+  hw_states_.resize(info_.joints.size(), std::numeric_limits<double>::quiet_NaN());
+  hw_commands_.resize(info_.joints.size(), std::numeric_limits<double>::quiet_NaN());
+
+  return CallbackReturn::SUCCESS;
+}
+
+hardware_interface::CallbackReturn RoboCartArduinoHW::on_configure(
+  const rclcpp_lifecycle::State & /*previous_state*/)
+{
+  // TODO(anyone): prepare the robot to be ready for read calls and write calls of some interfaces
+
+  return CallbackReturn::SUCCESS;
+}
+
+std::vector<hardware_interface::StateInterface> RoboCartArduinoHW::export_state_interfaces()
+{
+  std::vector<hardware_interface::StateInterface> state_interfaces;
+  for (size_t i = 0; i < info_.joints.size(); ++i)
+  {
+    state_interfaces.emplace_back(hardware_interface::StateInterface(
+      // TODO(anyone): insert correct interfaces
+      info_.joints[i].name, hardware_interface::HW_IF_POSITION, &hw_states_[i]));
+  }
+
+  return state_interfaces;
+}
+
+std::vector<hardware_interface::CommandInterface> RoboCartArduinoHW::export_command_interfaces()
+{
+  std::vector<hardware_interface::CommandInterface> command_interfaces;
+  for (size_t i = 0; i < info_.joints.size(); ++i)
+  {
+    command_interfaces.emplace_back(hardware_interface::CommandInterface(
+      // TODO(anyone): insert correct interfaces
+      info_.joints[i].name, hardware_interface::HW_IF_POSITION, &hw_commands_[i]));
+  }
+
+  return command_interfaces;
+}
+
+hardware_interface::CallbackReturn RoboCartArduinoHW::on_activate(
+  const rclcpp_lifecycle::State & /*previous_state*/)
+{
+  // TODO(anyone): prepare the robot to receive commands
+
+  return CallbackReturn::SUCCESS;
+}
+
+hardware_interface::CallbackReturn RoboCartArduinoHW::on_deactivate(
+  const rclcpp_lifecycle::State & /*previous_state*/)
+{
+  // TODO(anyone): prepare the robot to stop receiving commands
+
+  return CallbackReturn::SUCCESS;
+}
+
+hardware_interface::return_type RoboCartArduinoHW::read(
+  const rclcpp::Time & /*time*/, const rclcpp::Duration & /*period*/)
+{
+  // TODO(anyone): read robot states
+
+  return hardware_interface::return_type::OK;
+}
+
+hardware_interface::return_type RoboCartArduinoHW::write(
+  const rclcpp::Time & /*time*/, const rclcpp::Duration & /*period*/)
+{
+  // TODO(anyone): write robot's commands'
+
+  return hardware_interface::return_type::OK;
+}
+
+}  // namespace ros2_control_arduino_hw
+
+#include "pluginlib/class_list_macros.hpp"
+
+PLUGINLIB_EXPORT_CLASS(
+  ros2_control_arduino_hw::RoboCartArduinoHW, hardware_interface::SystemInterface)
